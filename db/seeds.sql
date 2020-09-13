c8a2b1ea09f16dcd0a966df88482b435756d6747
a-thread <aiden.threadgoode@gmail.com>
2020-09-13 16:32:21 -0400

    adding schema and seed file

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    burgers(burger_name, devoured)
+VALUES
+    ("BLACK BUN FRIED FROG BURGER", FALSE),
+    ("The Cronut Burger", FALSE),
+    ("BRAILLE BURGER", FALSE),
+    ("QUADRUPPLE BYPASS BURGER", FALSE),
+    ("Yorkie Pud Burger", FALSE)
