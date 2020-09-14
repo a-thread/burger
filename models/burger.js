b5c8fca9408e0be372ca368ed1f5feb466dc7b0c
a-thread <aiden.threadgoode@gmail.com>
2020-09-14 01:16:58 -0400

    burger.all model working

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm");
+
+var burger = {
+    all: function () {
+        orm.all().then((result) => {
+            console.log(result);
+        });
+    },
+    create: function (cols, vals) {
+        orm.create(cols, vals).then((result) => {
+            console.log(result);
+        });
+    },
+    update: function (condition) {
+        orm.delete(condition).then((result) => {
+            console.log(result);
+        });
+    }
+};
+
+module.exports = burger;
