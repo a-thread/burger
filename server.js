83b55718480097753507f14a16bc817c601e3a2a
a-thread <aiden.threadgoode@gmail.com>
2020-09-14 01:04:36 -0400

    reorganizing server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// dependencies
 var express = require("express");
 var routes = require("./controllers/burgers_controllers");
+var exphbs = require("express-handlebars");
 
+// Port
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -13,16 +16,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
+// starting server
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
+  // Log when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
